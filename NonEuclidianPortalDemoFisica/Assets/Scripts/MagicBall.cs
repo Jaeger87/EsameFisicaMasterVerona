5753b653447ebe865ec42fafd4a07059c72c27ea
Andrea <rosati.1595834@gmail.com>
2022-05-08 18:39:24 +0200

    Lancio palla meglio

--- NonEuclidianPortalDemoFisica/Assets/Scripts/MagicBall.cs
+++ NonEuclidianPortalDemoFisica/Assets/Scripts/MagicBall.cs
@@ -6,11 +6,13 @@ using UnityEngine;
 public class MagicBall : MonoBehaviour
 {
     private Rigidbody m_Rigidbody = null;
+    private SphereCollider m_Collider = null;
     [SerializeField] private float throwSpeed = 0;
     private bool flying = false;
     private void Start()
     {
         m_Rigidbody = GetComponent<Rigidbody>();
+        m_Collider = GetComponent<SphereCollider>();
     }
 
     // Update is called once per frame
@@ -24,6 +26,8 @@ public class MagicBall : MonoBehaviour
         transform.parent = null;
         m_Rigidbody.isKinematic = false;
         m_Rigidbody.useGravity = true;
+        m_Collider.isTrigger = false;
+        m_Collider.radius = 0.5f;
         flying = true;
         m_Rigidbody.AddForce((forwardDirection * throwSpeed) + Vector3.up * 5, ForceMode.Impulse);
     }
