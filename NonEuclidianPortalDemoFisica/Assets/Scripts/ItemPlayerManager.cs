7886f4f413abc50daff80595f9019e1ccb8347a3
Andrea <rosati.1595834@gmail.com>
2022-05-08 18:22:25 +0200

    Lancio ok

--- NonEuclidianPortalDemoFisica/Assets/Scripts/ItemPlayerManager.cs
+++ NonEuclidianPortalDemoFisica/Assets/Scripts/ItemPlayerManager.cs
@@ -5,22 +5,32 @@ using UnityEngine;
 
 public class ItemPlayerManager : MonoBehaviour
 {
-    private GameObject itemGrabbed = null;
+    private MagicBall itemGrabbed = null;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(itemGrabbed != null)
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                itemGrabbed.throwAway(transform.forward);
+            }
     }
 
     void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Item")
         {
-            Debug.Log("tri");
-            itemGrabbed = other.gameObject;
-            itemGrabbed.transform.parent = transform;
-            itemGrabbed.transform.localPosition = new Vector3(0f, 0.3f, 1f);
+            MagicBall mb = other.gameObject.GetComponent<MagicBall>();
+            if (!mb.isFlying())
+            {
+                itemGrabbed = other.gameObject.GetComponent<MagicBall>();
+                itemGrabbed.transform.parent = transform;
+                itemGrabbed.transform.localPosition = new Vector3(0f, 0.3f, 1f);
+            }   
+
         }
     }
+    
+    
 }
--- NonEuclidianPortalDemoFisica/Assets/Scripts/MagicBall.cs
+++ NonEuclidianPortalDemoFisica/Assets/Scripts/MagicBall.cs
@@ -1,13 +1,40 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MagicBall : MonoBehaviour
 {
+    private Rigidbody m_Rigidbody = null;
+    [SerializeField] private float throwSpeed = 0;
+    private bool flying = false;
+    private void Start()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+    }
 
     // Update is called once per frame
     void Update()
     {
         transform.Rotate(0,0.8f,0);
+    }
+
+    public void throwAway(Vector3 forwardDirection)
+    {
+        transform.parent = null;
+        m_Rigidbody.isKinematic = false;
+        m_Rigidbody.useGravity = true;
+        flying = true;
+        m_Rigidbody.AddForce((forwardDirection * throwSpeed) + Vector3.up * 5, ForceMode.Impulse);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Collisione");
+    }
+
+    public bool isFlying()
+    {
+        return flying;
     }
 }
